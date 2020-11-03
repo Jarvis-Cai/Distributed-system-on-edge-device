3c498c9def361444e58e5bb303af7b3c32128867
hb0019 <55646840+hb0019@users.noreply.github.com>
2020-11-03 15:18:36 +0800

    Update auto_check.py
    Add this file notes

--- auto_check.py
+++ auto_check.py
@@ -1,6 +1,8 @@
 # author: caijiawei
-# time: 2020.3.10
+# assistant：HuangBin
+# time: 2020.11.3
 # officially!
+# Fault self-detection module (token)
 
 import socket
 import time
@@ -22,8 +24,8 @@ class AutoCheck:
 	Among all devices, at most one device's start_flag be true. And that means start from inside.
 	"""
 	def __init__(self, ring_key=None, start_flag=False, size_of_queue=10):
-
-		self.q = Queue(maxsize=size_of_queue)
+		self.PingLogFile = open('./ping_log.txt','w')
+		self.q = Queue(maxsize=size_of_queue)	# create queue，queue max size is 10
 		if start_flag:
 			self.q.put(ring_key)
 		self.local_ip = self.get_local_ip_address()
@@ -34,25 +36,29 @@ class AutoCheck:
 		check local ip address
 		:return:
 		"""
+		# used udp to get local ip
 		try:
 			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-			s.connect(('8.8.8.8', 80))
-			ip = s.getsockname()[0]
+			s.connect(('8.8.8.8', 80))							
+			ip = s.getsockname()[0]								
 		finally:
 			s.close()
 		return ip
 
-	@staticmethod
-	def judge_if_a_ip_can_reach(certain_ip):
+#	@staticmethod
+	def judge_if_a_ip_can_reach(self,certain_ip):
 		"""
 		given a certain ip, judge whether it can be reached
 		:param certain_ip:a ip address
 		:return:Ture or False
 		"""
-		if subprocess.call(["ping", "-c", "2", certain_ip]) == 0 or \
-				subprocess.call(["ping", certain_ip]) == 0:  # send two ECHO_REQUEST package
+		# judice certain_ip is ping or not 
+		if subprocess.call(["ping", "-c", "2", certain_ip],stdout=self.PingLogFile) == 0 or \
+				subprocess.call(["ping", certain_ip],stdout=self.PingLogFile) == 0:  # send two ECHO_REQUEST package
+			print("==" * 5 ,certain_ip," is success connected!", "==" * 5)
 			return True
 		else:
+			print("==" * 5 ,"Fail to connect",certain_ip, "==" * 5)
 			return False
 
 	def listener(self):
@@ -60,13 +66,14 @@ class AutoCheck:
 		the func is used to listen port 22222
 		:return: updated _key_ip_address
 		"""
-		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		# udp listener
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)	
 		sock.bind(('0.0.0.0', 22222))
 		while True:
-			ret, addr = sock.recvfrom(1024)
+			ret, addr = sock.recvfrom(1024)	
 			if ret:
-				ret = json.loads(ret)
-				self.q.put(ret)
+				ret = json.loads(ret)							
+				self.q.put(ret)									# if data is valid, put data(ret) in queue
 
 	@staticmethod
 	def send_a_message(a_message, next_one_ip):
@@ -78,26 +85,29 @@ class AutoCheck:
 		"""
 		time.sleep(3)  # take care of the delay time
 		udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		dest_addr = (next_one_ip, 22222)
-		json_string = json.dumps(a_message).encode()
-		udp_socket.sendto(json_string, dest_addr)
+		dest_addr = (next_one_ip, 22222)						# ip of next device 
+		json_string = json.dumps(a_message).encode()			# package the token information
+		udp_socket.sendto(json_string, dest_addr)				# udp sent
 		udp_socket.close()
 		print('message sending finished!')
 
+
 	def first_start(self):
-		sendcommand_instance = sas.SendCommand()
+		# System operating state transition function
+		sendcommand_instance = sas.SendCommand()	# defince start_all_system.py->SendCommand()
 		while True:
-			_key_ring = self.q.get()
-			ip_of_devices = _key_ring['device']
+			_key_ring = self.q.get()				# take out one information in queue :ring_key (system run initial process will block in here)
+			ip_of_devices = _key_ring['device']		# ip information of system's each device
 			# 'device': [['192.168.0.100', 0.0], ['192.168.0.107', 0.0], ['192.168.0.105', 0.0]]
 			Nums_of_devices = len(ip_of_devices)  # nums of devices at present
-			situation_of_system = _key_ring['situation']
+			situation_of_system = _key_ring['situation']	# Operating information at the moment
 			# 'situation': {'flag_of_ok_device_sum': 0, 'system_is_running': False}
-			flag_of_ok_device_sum = situation_of_system['flag_of_ok_device_sum']
-			system_is_running = situation_of_system['system_is_running']
+			flag_of_ok_device_sum = situation_of_system['flag_of_ok_device_sum']	# ready device number information
+			system_is_running = situation_of_system['system_is_running']			# local device ready information
 			# judge if 'flag_of_bit' is all ok
 
 			if flag_of_ok_device_sum == Nums_of_devices and not system_is_running:
+				# strat to the distribute system
 				# launch branch
 				_key_ring['situation']['system_is_running'] = True
 				sendcommand_instance.start_system(_key_ring)  # start distribute system
@@ -105,6 +115,7 @@ class AutoCheck:
 				next_one_ip, _ = self._get_next_one_ip(ip_of_devices)
 				self.send_a_message(_key_ring, next_one_ip)
 			elif flag_of_ok_device_sum < Nums_of_devices:
+				# Preparation Phase, to wake up next device 
 				# calculate brach
 				# find out yourself and get the capability
 				next_one_ip, _ = self._get_next_one_ip(ip_of_devices, if_calculate=True)
@@ -113,35 +124,41 @@ class AutoCheck:
 				print(_key_ring, '\n prepare for starting!')  # test del
 				self.send_a_message(_key_ring, next_one_ip)
 			elif system_is_running:
+				# Operating phase, token information is continuously updated
 				# running branch
 				next_one_ip, del_one_ip = self._get_next_one_ip(ip_of_devices)
 				if del_one_ip and del_one_ip[0] == _key_ring['server'][0]:
+					# while server Dropped, system should be restart
 					# restart all system
 					sendcommand_instance.stop_system()
 					time.sleep(3)
 					print('restart training system!!!', _key_ring)
 					sendcommand_instance.start_system(_key_ring)
 				else:
-					print(_key_ring, '\n is running')  # just for test
+					# worker Dropped, del it in _key_ring
+					print('following cluster system is running \n' , _key_ring)  # just for test
 				self.send_a_message(_key_ring, next_one_ip)
 
+	# search the next worker from token
+	# return：next_one_ip
+	#		  del_one_ip
 	def _get_next_one_ip(self, ip_of_devices, if_calculate=False):
-		Nums_of_devices = len(ip_of_devices)  # nums of devices at present
-		for i, c in enumerate(ip_of_devices):
-			if c[0] == self.local_ip:  # found!
+		Nums_of_devices = len(ip_of_devices)  	# nums of devices at present
+		for i, c in enumerate(ip_of_devices): 	# i：Numbering，c：device's ip
+			if c[0] == self.local_ip:  			# Find the number position of the IP number of the current device！
 				if if_calculate:
-					c[1] = cst.test_calculate_time()  # calculate time
+					c[1] = cst.test_calculate_time()  # calculate time (Computing performance)----
 				i += 1  # next one
 				i %= Nums_of_devices
 				print(ip_of_devices)  # del
-				next_one_ip = ip_of_devices[i][0]
+				next_one_ip = ip_of_devices[i][0]		# sucess find next device ip
 				break
 
 		del_one_ip = None
-		while not (self.judge_if_a_ip_can_reach(next_one_ip)):
+		while not (self.judge_if_a_ip_can_reach(next_one_ip)):	# while can not connect next device, it should do something
 			del_one_ip = ip_of_devices[i]
 			print('remove ', ip_of_devices[i])
-			del ip_of_devices[i]  # here add switch server function
+			del ip_of_devices[i]  # ++++++++ here add switch server function ++++++++ #
 			if i == len(ip_of_devices):  # if i is the last of in the list.
 				i = 0
 			next_one_ip = ip_of_devices[i][0]
@@ -149,11 +166,11 @@ class AutoCheck:
 		return next_one_ip, del_one_ip
 
 	def run(self):
-		t1 = Thread(target=self.listener, args=())
-		t2 = Thread(target=self.first_start, args=())
+		t1 = Thread(target=self.listener, args=())					
+		t2 = Thread(target=self.first_start, args=())				
 		t1.start()
 		t2.start()
-		print(self.__class__.__name__, 'start success!')
+		print(self.__class__.__name__, 'start success!')			# AutoCheck start success!
 
 
 if __name__ == '__main__':
