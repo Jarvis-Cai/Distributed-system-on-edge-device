a0a3713271414878fb53de29e8b20f7d5b66851d
hb0019 <55646840+hb0019@users.noreply.github.com>
2020-11-03 15:20:30 +0800

    Update calculate_speed_test.py
    Add this file notes

--- calculate_speed_test.py
+++ calculate_speed_test.py
@@ -1,13 +1,14 @@
 # author:caijiawei
-# time:2020.3.9
+# assistant：HuangBin
+# time:2020.11.3
 # this file is to test every single device's computing capability. So we will run a simple neural network and return the
 # time needed.
+# Computing performance test module
 
 import tensorflow as tf
 import cnn_model
 import time
 import numpy as np
-
 
 def test_calculate_time():
 	start_time = time.time()
@@ -24,13 +25,15 @@ def test_calculate_time():
 	y = cnn_model.cnn_test(x, 10)
 
 	# graph end
+	# logits：last layer, shape is[batchsize,num_classes]
+	# labels： true labels
 	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-
 	opt = tf.train.AdamOptimizer(0.001)
-
 	train = opt.minimize(cost)
 
+	# Determine whether the prediction is correct
 	correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+	# Correct rate
 	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 	# local init_op created
 	init_op = tf.global_variables_initializer()
@@ -38,6 +41,8 @@ def test_calculate_time():
 	with tf.Session() as sess:
 		sess.run(init_op)
 		sess.run([train, cost], feed_dict={x: data, y_: label})
+
+	# Get inference test running time
 	total_time_consumption = time.time() - start_time
 	return round(total_time_consumption, 2)
 
